3674df5315991ef1b42f17dc6ec8a945e0e2ed73
Sankar Manoj <smanoj@tenstorrent.com>
2025-04-01 09:28:08 +0000

    #0: Updated tests for conv dram

--- tests/ttnn/nightly/unit_tests/operations/conv/test_conv2d.py
+++ tests/ttnn/nightly/unit_tests/operations/conv/test_conv2d.py
@@ -95,6 +95,8 @@ def run_conv(
     preprocess_weights_on_device=True,
     in_place=False,
     run_twice=False,
+    fast_compare=False,
+    slice_config=None,
 ):
     if isinstance(device, ttnn.MeshDevice):
         assert input_mesh_mapper is not None, "Expected mesh mapper for input tensor when using device mesh"
@@ -116,6 +118,8 @@ def run_conv(
         pytest.xfail(
             "Untilize_out is not supported when out_c > 256 for Height Sharded. https://github.com/tenstorrent/tt-metal/issues/18633"
         )
+    if slice_config and activations_dtype != ttnn.bfloat16:
+        pytest.xfail("Conv2d with DRAM Slicing only supports BFloat16 for activation dtype")
 
     if hasattr(padding, "__len__"):
         if len(padding) == 2:
@@ -152,7 +156,7 @@ def run_conv(
         mode="constant",
         value=0,
     )
-    torch_out_golden_tensor = torch.nn.functional.conv2d(
+    ref = torch.nn.functional.conv2d(
         torch_padded_input,
         torch_weight_tensor,
         bias=torch_bias_tensor.reshape(-1) if has_bias else None,
@@ -163,7 +167,7 @@ def run_conv(
     )
     act_func = get_torch_act_func_from_string(activation)
     if act_func:
-        torch_out_golden_tensor = act_func(torch_out_golden_tensor)
+        ref = act_func(ref)
 
     reader_patterns_cache = {}
 
@@ -179,12 +183,14 @@ def run_conv(
             weights_dtype if weights_dtype != ttnn.bfloat8_b else ttnn.float32,
             mesh_mapper=weight_mesh_mapper,
         )
-
-    tt_input_tensor = ttnn.from_torch(
-        torch_input_tensor,
-        activations_dtype if activations_dtype == ttnn.float32 else ttnn.bfloat16,
-        mesh_mapper=input_mesh_mapper,
-    )
+    if slice_config:
+        tt_input_tensor = ttnn.from_torch(torch_input_tensor, device=device, dtype=ttnn.bfloat16)
+    else:
+        tt_input_tensor = ttnn.from_torch(
+            torch_input_tensor,
+            activations_dtype if activations_dtype == ttnn.float32 else ttnn.bfloat16,
+            mesh_mapper=input_mesh_mapper,
+        )
 
     conv_config = ttnn.Conv2dConfig(
         dtype=activations_dtype,
@@ -199,7 +205,7 @@ def run_conv(
         activation=activation,
         transpose_shards=transpose_shards,
         preprocess_weights_on_device=preprocess_weights_on_device,
-        always_preprocess_weights=True,
+        always_preprocess_weights=False,
         in_place=in_place,
     )
     compute_config = ttnn.init_device_compute_kernel_config(
@@ -240,6 +246,7 @@ def run_conv(
         debug=debug,
         groups=groups,
         memory_config=memory_config,
+        slice_config=slice_config,
         return_output_dim=True,
         return_weights_and_bias=True,
     )
@@ -264,19 +271,18 @@ def run_conv(
             debug=debug,
             groups=groups,
             memory_config=memory_config,
+            slice_config=slice_config,
             return_output_dim=True,
             return_weights_and_bias=True,
         )
     tt_output_tensor = ttnn.from_device(tt_output_tensor_on_device)
-    torch_output_tensor = ttnn.to_torch(tt_output_tensor, mesh_composer=output_mesh_composer)
-    # torch_output_tensor is in row major layout and NHWC shape
+    out = ttnn.to_torch(tt_output_tensor, mesh_composer=output_mesh_composer)
+    # out is in row major layout and NHWC shape
     # NHWC to NCHW
-    torch_output_tensor = torch_output_tensor.reshape(
-        total_batch_size, out_height, out_width, torch_output_tensor.shape[-1]
-    )
-    torch_output_tensor = torch_output_tensor[:, :, :, :output_channels]
+    out = out.reshape(total_batch_size, out_height, out_width, out.shape[-1])
+    out = out[:, :, :, :output_channels]
 
-    torch_output_tensor = torch.permute(torch_output_tensor, (0, 3, 1, 2))
+    out = torch.permute(out, (0, 3, 1, 2))
     reader_patterns_cache.clear()
 
     if not fp32_accum:
@@ -293,9 +299,35 @@ def run_conv(
         # tanh has a range of -1 to 1. So discrepancies in output values which are close to 0 tend to disproportionately affect the PCC.
         pcc = pcc * 0.99
 
-    passing, pcc_msg = check_with_pcc_without_tensor_printout(torch_output_tensor, torch_out_golden_tensor, pcc=pcc)
-    logger.info(f"PCC = {pcc_msg}. Threshold = {pcc}")
-    assert passing, pcc_msg
+    scaled_diff_mean = 0
+    if fast_compare:
+        diff = torch.abs(out - ref).flatten()
+        if torch.any(diff.isnan()) or torch.any(diff.isinf()):
+            # Histogram doesn't work if there are any infs or nans.
+            fast_compare = False
+            logger.info("Fast compare disabled due to inf or nan in diff")
+    if fast_compare and not torch.any(diff.isnan()) and not torch.any(diff.isinf()):
+        num_elements = diff.numel()
+        # Filter out the top 1% of the differences.
+        # Using histogram to find the top 1% of differences is faster than sorting. Sorting is single-core.
+        bin_count, bin_edge = torch.histogram(diff, bins=100)
+        top1_count = num_elements // 100
+        count = 0
+        index = -1
+        while count < top1_count and index < len(bin_count) - 1:
+            count += bin_count[index]
+            index -= 1
+        top1_diff = diff[diff > bin_edge[index]]
+
+        abs_ref = ref.abs()
+        abs_ref_mean = abs_ref.mean()
+
+        scaled_diff_mean = top1_diff.mean() / abs_ref_mean
+        logger.info(f"Filtered Scaled diff mean = {scaled_diff_mean} ")
+    if not fast_compare or scaled_diff_mean > 0.2:
+        passing, pcc_msg = check_with_pcc_without_tensor_printout(out, ref, pcc=pcc)
+        logger.info(f"PCC = {pcc_msg}. Threshold = {pcc}")
+        assert passing, pcc_msg
 
     if memory_config:
         output_memory_config = ttnn.get_memory_config(tt_output_tensor_on_device)
