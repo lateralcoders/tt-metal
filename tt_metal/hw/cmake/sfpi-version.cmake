140c9f471a57f507d67d89aa9031e38307035073
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-04 17:07:45 +0000

    #0: sfpi cleanup

--- tt_metal/hw/cmake/sfpi-version.cmake
+++ tt_metal/hw/cmake/sfpi-version.cmake
@@ -1,6 +1,6 @@
 # Define the SFPI version to use
 
 set(SFPI_x86_64_Linux_RELEASE
-    "v6.8.0/sfpi-release.tgz"
-    "fca4cea73901483f741f6f325c289c7d"
+    "v6.8.0-cleanup/sfpi-x86_64-Linux.tgz"
+    "0a3f308e7a6c4009122165c3403b76ae"
 )
